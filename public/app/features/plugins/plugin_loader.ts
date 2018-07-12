openai-oss-forks/grafana-experiments

commit 7ee333c5a8d11bad048a7211c91c065c4694a6f0

Fix css loading in plugins (#12573)
- allow css loader to be imported again (wasnt prefixed by plugin)

public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    'plugin*': {
+    '*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',

commit 070f555d7893d5f5ab4703b783f92b79aaa85d06

Fix freezing browser when loading plugin
- broken since 7ee333c5a
- `*` was previously working as a path matcher, but freezes browser when
  used with new cache-busting plugin loader
- changed matcher to be `/*`

public/app/features/plugins/plugin_loader.ts
@@ -56,7 +56,7 @@ System.config({
     css: 'vendor/plugin-css/css.js',
   },
   meta: {
-    '*': {
+    '/*': {
       esModule: true,
       authorization: true,
       loader: 'plugin-loader',